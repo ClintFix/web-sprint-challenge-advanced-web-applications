e5e855384cfe3827ac7cd30dce4f189ae1c80122
ClintFix <clint.fix@gmail.com>
2021-03-26 11:31:00 -0600

    testing done

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
+import mockFetchColors from '../api/fetchColors'
 import BubblePage from "./BubblePage";
 
+jest.mock('../api/fetchColors')
+
 test("Renders BubblePage without errors", () => {
+  mockFetchColors.mockResolvedValueOnce(testColor)
   render(<BubblePage/>)
 });
 
+const testColor = [{
+  color: 'blue',
+  code: {hex: '#0000ff'},
+  id: 1
+}]
+
 test("Fetches data and renders the bubbles on mounting", async () => {
-  // Finish this test
-  // Arrange: Render BubblePage
-    render(<BubblePage />)
-  // act: none
-  // assert: that bubbles are mounted
-    const bubbles = await screen.findByText('bubbles')
-    expect(bubbles).toBeInTheDocument();
+  
+  mockFetchColors.mockResolvedValueOnce(testColor)
+  render(<BubblePage />)
+
+  const bubbles = await screen.findByTestId('bubble-container')
+  expect(bubbles).toBeInTheDocument();
 });
 
 //Task List
